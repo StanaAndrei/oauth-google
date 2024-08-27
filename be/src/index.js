9551cd32af326f8439dc187ecc23e13fb807556e
StanaAndrei <stadey33@gmail.com>
2024-08-28 01:07:27 +0300

    init fe

--- be/src/index.js
+++ be/src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 
 require('dotenv').config();
 //require('./passport.cfg')
@@ -10,9 +11,15 @@ const app = express();
 
 app.use(bodyParser.json())
 app.use(passport.initialize())
+app.use(
+	cors({
+		// origin: "http://localhost:3000",
+		// methods: "GET,POST,PUT,PATCH,DELETE",
+		// credentials: true,
+	})
+);
 
-
-const HTTP_PORT = 3000;
+const HTTP_PORT = 3001;
 app.listen(HTTP_PORT, () => {
     console.log(`Listening on port ${HTTP_PORT}!`);
 })
