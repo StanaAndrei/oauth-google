0769c3226a2a8cdb0f4e776d1ff20859ce6a27f8
StanaAndrei <stadey33@gmail.com>
2024-08-28 23:23:26 +0300

    added links

--- fe/src/pages/Home/Home.jsx
+++ fe/src/pages/Home/Home.jsx
@@ -3,6 +3,15 @@ function Home() {
   return (
     <div>
       Home
+      <p>
+        A simple app to prove google using oauth2!
+      </p>
+      <p>
+        <a href="/signin">Sign In</a>
+      </p>
+      <p>
+        <a href="/signup">Sign Up</a>
+      </p>
     </div>
   );
 }
--- fe/src/pages/SignIn/SignIn.jsx
+++ fe/src/pages/SignIn/SignIn.jsx
@@ -8,7 +8,6 @@ function SignIn() {
   
   const handleSubmit = e => {
     e.preventDefault()
-    console.log(email, password);
     axiosInstToSv.post('/user/signin', {
       email, password
     }).then(res => {
